d770ee84c758a19290bbf438590a6a12fbcfe1bb
Braian <braianvoficial@gmail.com>
2022-08-21 16:04:23 -0300

    updating data

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,13 +10,11 @@ export const projects = [
     {id: 1, title: 'ReactFlix', description: 'A project made with react typescript using styled-components', image: reactflix, link: 'https://react-flix-bbraian.vercel.app/', 
         tecnologies: [
             {id: 1, name: 'ReactJS', icon: react},
-            {id: 2, name: 'NodeJS', icon: javascript},
         ]
     },
-    {id: 2, title: 'Todo list', description: 'Todo is an react-app made with typescript. Is the convencional list of things to do. You can create, and mark as done', image: imc, link: 'https://todo-react-list.vercel.app/', 
+    {id: 2, title: 'IMC App', description: 'This project is a simple IMC calculator app made with React Native', image: imc, link: 'https://todo-react-list.vercel.app/', 
         tecnologies: [
-            {id: 3, name: 'PHP', icon: php},
-            {id: 4, name: 'MySQL', icon: mysql}
+            {id: 3, name: 'ReactJS', icon: react},
         ]
     },
     {id: 3, title: 'Unisinos Groups', description: 'This project is basically a list to store the whatsapp groups of the classes from college', image: unisinos, link: 'https://unisinos-groups.vercel.app/', 
